97f14604b45a720f0c32411f7877ffe299cf2d1b
Shaleen Agarwal <shaleen.agarwal@wakefit.co>
2021-10-11 02:49:31 +0530

    Updates User routes and controllers

--- User/src/User/user.controllers.js
+++ User/src/User/user.controllers.js
@@ -2,7 +2,6 @@ import { User } from './user.model'
 
 const createUser = async (req, res) => {
   try {
-    // eslint-disable-next-line no-unused-vars
     const user = await User.create(req.body)
     res.status(201).send(user.toJSON())
   } catch (e) {
@@ -61,13 +60,29 @@ const getUser = async (req, res) => {
     res.status(200).json(user)
   }
 }
+const updateNumber = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { userID: req.body.userID },
+      { phoneNumber: req.body.phoneNumber },
+      { new: true, lean: true }
+    ).exec()
+
+    return res.status(200).json(user)
+  } catch (e) {
+    console.log('error is ')
+    console.error(e)
+    return res.status(404).send('User Not found')
+  }
+}
 
 const userControllers = {
   createUser,
   getAllUsers,
   getUser,
   updateName,
-  deleteUser
+  deleteUser,
+  updateNumber
 }
 
 export default userControllers
